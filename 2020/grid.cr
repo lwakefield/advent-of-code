fc4c24975e8c263eba23c7308d0c7385a7b6134e
Lawrence Wakefield <lawrence@iamlawrence.me>
2020-11-28 10:24:50 -0500

    Add more utils

---
+++ 2020/grid.cr
@@ -0,0 +1,50 @@
+class Grid(T)
+	@grid : Hash(Tuple(Int32, Int32), T)
+	@width : Int32
+	@height : Int32
+
+	def initialize(@width : Int32, @height : Int32)
+		@grid = Hash(Tuple(Int32, Int32), T).new
+		@width.times do |x|
+			@height.times do |y|
+				@grid[{x, y}] = yield x, y
+			end
+		end
+	end
+
+	def []? (x : Int32, y : Int32)
+		nil if x < 0
+		nil if x >= @width
+		nil if y < 0
+		nil if y >= @height
+
+		@grid[{x, y}]
+	end
+
+	def [] (x : Int32, y : Int32)
+		val = self[x, y]?
+		raise IndexError.new if val.nil?
+		val
+	end
+
+	def []= (x : Int32, y : Int32, v : T)
+		@grid[{x,y}] = v
+	end
+
+	def each
+		@height.times do |y|
+			@width.times do |x|
+				yield x, y, self[x, y]
+			end
+		end
+	end
+
+	def to_s (io : IO)
+		@height.times do |y|
+			@width.times do |x|
+				io << self[x, y]
+			end
+			io << '\n'  if y != @height - 1
+		end
+	end
+end
