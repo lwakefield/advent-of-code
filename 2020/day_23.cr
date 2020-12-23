1ee26b788d7001d0ec76e03a8116e6c8c25bfca7
Lawrence Wakefield <lawrence@thirtymadison.com>
2020-12-23 13:43:15 -0500

    Final optimized but maybe still naive day 23 soln

--- 2020/day_23.cr
+++ 2020/day_23.cr
@@ -1,46 +1,139 @@
 require "time"
-cups = Deque.new (ARGV.first? || "523764819").chars.map(&.to_i)
+cups = (ARGV.first? || "523764819").chars.map(&.to_i)
 
+class LinkedList
+  @lut = {} of Int32 => LinkedListNode
+  getter front : LinkedListNode?
+  getter back : LinkedListNode?
 
-def solve(cups : Deque(Int32), iterations)
+  def unshift(node)
+    if !front
+      @back = node
+      @front = node
+      return
+    end
+
+    @front.not_nil!.left = node
+    node.right = @front
+    @front = node
+
+    @back = @front if @back.nil?
+  end
+
+  def push(node)
+    if !back
+      @back = node
+      @front = node
+      return
+    end
+
+    @back.not_nil!.right = node
+    node.left = @back
+    @back = node
+  end
+
+  def shift
+    node = @front.not_nil!
+    node.right.not_nil!.left = nil if node.right
+    @front = node.right
+    node.right = nil
+    node.left = nil
+    node
+  end
+
+  def each
+    node = @front
+    while node
+      yield node
+      node = node.right
+    end
+  end
+
+  def insert_right (node, to_insert)
+    node.right.not_nil!.left = to_insert if node.right
+    to_insert.right = node.right
+    to_insert.left = node
+    node.right = to_insert
+
+    @back = to_insert if to_insert.right.nil?
+  end
+
+  def rotate!
+    push(shift)
+  end
+end
+
+class LinkedListNode
+  property left : LinkedListNode?
+  property right : LinkedListNode?
+  getter val
+
+  def initialize(@val : Int32)
+  end
+end
+
+def solve(cups : LinkedList, range, iterations)
   start = Time.local
-  min, max = cups.min, cups.max
+  lut = {} of Int32 => LinkedListNode
+  cups.each do |n|
+    lut[n.val] = n
+  end
 
   iterations.times do |i|
     puts "#{(Time.local - start).total_seconds}s #{i}" if i % 1000 == 0
     current_cup = cups.shift
     three_cups = [
-        cups.shift,
-        cups.shift,
-        cups.shift,
+      cups.shift,
+      cups.shift,
+      cups.shift,
     ]
+    three_cup_vals = three_cups.map(&.val)
 
-    destination = current_cup
-    until destination != current_cup && !three_cups.includes?(destination)
-      destination -= 1
-      destination = max if destination < min
+    destination_val = current_cup.val
+    until destination_val != current_cup.val && !three_cup_vals.includes?(destination_val)
+      destination_val -= 1
+      destination_val = range.end if destination_val < range.begin
     end
 
-    destination_index = cups.index(destination).not_nil!
-    cups.insert destination_index + 1, three_cups[2]
-    cups.insert destination_index + 1, three_cups[1]
-    cups.insert destination_index + 1, three_cups[0]
+    destination = lut[destination_val]
+    cups.insert_right destination, three_cups[2]
+    cups.insert_right destination, three_cups[1]
+    cups.insert_right destination, three_cups[0]
     cups.push(current_cup)
   end
 
-  until cups.first == 1
+  until cups.front.not_nil!.val == 1
     cups.rotate!
   end
 
   cups
 end
 
-cups_part_1 = solve(cups.dup, 100)
-puts "part 1 cups=#{cups_part_1}"
+cups_part_1 = LinkedList.new
+cups.each do |n|
+  cups_part_1.push(LinkedListNode.new(n))
+end
 
-# min, max = cups.min, cups.max
+solve(cups_part_1, (1..9), 100)
+res = [] of Int32
+cups_part_1.each do |n|
+    res << n.val
+end
 
-cups_part_2 = solve(cups.dup + Deque.new((10..1_000_000).map(&.to_i)), 10_000_000)
+puts "part 1 cups=#{res}"
 
-puts "part 2 slice=[#{cups_part_2[0]}, #{cups_part_2[1]}, #{cups_part_2[2]}]"
-puts "part 2 ans=#{cups_part_2}"
+cups_part_2 = LinkedList.new
+cups.each do |n|
+  cups_part_2.push(LinkedListNode.new(n))
+end
+(10..1_000_000).each do |n|
+  cups_part_2.push(LinkedListNode.new(n))
+end
+solve(cups_part_2, (1..1_000_000), 10_000_000)
+
+res = [] of Int32
+cups_part_2.each do |n|
+    res << n.val
+end
+
+puts "part 2 cups=#{res[0..2]}"
