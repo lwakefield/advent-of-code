d924086df378fc900ddf0c57ea4dbef450b0848d
Lawrence Wakefield <lawrence@thirtymadison.com>
2022-12-02 06:07:17 -0500

    Tidy up

--- 2022/day02.cr
+++ 2022/day02.cr
@@ -1,41 +1,40 @@
-their_map = { "A" => "r", "B" => "p", "C" => "s" }
-our_map   = { "X" => "r", "Y" => "p", "Z" => "s" }
-
-input = STDIN.gets_to_end.lines
+input = STDIN.gets_to_end.lines.map{|l| Tuple(String,String).from(l.split " ")}
 
 score = 0
-input.each do |line|
-  them, us = line.split " "
-
-  case {their_map[them], our_map[us]}
-  when {"r", "p"}, {"p", "s"}, {"s", "r"} then score += 6
-  when {"r", "r"}, {"p", "p"}, {"s", "s"} then score += 3
-  when {"r", "s"}, {"p", "r"}, {"s", "p"} then score += 0
-  end
-
-  case our_map[us]
-  when "r" then score += 1
-  when "p" then score += 2
-  when "s" then score += 3
+input.each do |them, us|
+  case {them, us}
+  # losses
+  when {"A", "Z"} then score += 0 + 3
+  when {"B", "X"} then score += 0 + 1
+  when {"C", "Y"} then score += 0 + 2
+  # draws
+  when {"A", "X"} then score += 3 + 1
+  when {"B", "Y"} then score += 3 + 2
+  when {"C", "Z"} then score += 3 + 3
+  # wins
+  when {"A", "Y"} then score += 6 + 2
+  when {"B", "Z"} then score += 6 + 3
+  when {"C", "X"} then score += 6 + 1
   end
 end
 
 puts "part 1: #{score}"
 
 score = 0
-input.each do |line|
-  them, outcome = line.split " "
-
-  case {outcome, their_map[them]}
-  when {"X", "r"} then score += 0 + 3
-  when {"X", "p"} then score += 0 + 1
-  when {"X", "s"} then score += 0 + 2
-  when {"Y", "r"} then score += 3 + 1
-  when {"Y", "p"} then score += 3 + 2
-  when {"Y", "s"} then score += 3 + 3
-  when {"Z", "r"} then score += 6 + 2
-  when {"Z", "p"} then score += 6 + 3
-  when {"Z", "s"} then score += 6 + 1
+input.each do |them, outcome|
+  case {outcome, them}
+  # losses
+  when {"X", "A"} then score += 0 + 3
+  when {"X", "B"} then score += 0 + 1
+  when {"X", "C"} then score += 0 + 2
+  # draws
+  when {"Y", "A"} then score += 3 + 1
+  when {"Y", "B"} then score += 3 + 2
+  when {"Y", "C"} then score += 3 + 3
+  # wins
+  when {"Z", "A"} then score += 6 + 2
+  when {"Z", "B"} then score += 6 + 3
+  when {"Z", "C"} then score += 6 + 1
   end
 end
 
