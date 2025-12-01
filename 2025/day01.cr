2cb2739d755f4f84027eab4043626282e309fb75
Lawrence Wakefield <lawrence@thirtymadison.com>
2025-12-01 17:03:20 +0000

    solved day01

---
+++ 2025/day01.cr
@@ -0,0 +1,34 @@
+instrs = STDIN.gets_to_end.lines
+
+dial = 50
+passwd = 0
+instrs.each do |inst|
+    _, dir, amt = inst.match! /(L|R)(\d+)/
+    dir = dir == "R" ? 1 : -1
+    amt = amt.to_i
+
+    dial += (dir * amt)
+    until dial < 100
+        dial -= 100
+    end
+    until dial >= 0
+        dial += 100
+    end
+
+    passwd += 1 if dial == 0
+end
+puts "part 1: #{passwd}"
+
+dial = 50
+passwd = 0
+instrs.each do |inst|
+    _, dir, amt = inst.match! /(L|R)(\d+)/
+    dir = dir == "R" ? 1 : -1
+    amt.to_i.times do
+        dial += dir
+        dial = 0 if dial == 100
+        dial = 99 if dial == -1
+        passwd += 1 if dial == 0
+    end
+end
+puts "part 2: #{passwd}"
