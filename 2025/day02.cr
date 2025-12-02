91bff9bfb1390d099a328d2b436f2e6a7e5fa567
Lawrence Wakefield <lawrence@thirtymadison.com>
2025-12-02 13:44:30 +0000

    solved day02

---
+++ 2025/day02.cr
@@ -0,0 +1,35 @@
+ranges = STDIN.gets_to_end.split(",").map do |r|
+    start, stop = r.split "-"
+    (start.to_i64 .. stop.to_i64)
+end
+
+def is_repeating_p1? (str)
+    str[0...str.size//2] * 2 == str
+end
+
+def is_repeating_p2? (str)
+    (1..str.size//2).each do |i|
+        return true if (str[0...i] * (str.size // i)) == str
+    end
+end
+
+# pp is_repeating?("12345")
+# pp is_repeating?("1010")
+# pp is_repeating?("55")
+# pp is_repeating?("11")
+
+sum = 0i64
+ranges.each do |r|
+    r.each do |id|
+        sum += id if is_repeating_p1?(id.to_s)
+    end
+end
+puts "part 1: #{sum}"
+
+sum = 0i64
+ranges.each do |r|
+    r.each do |id|
+        sum += id if is_repeating_p2?(id.to_s)
+    end
+end
+puts "part 1: #{sum}"
