b7accbf13cb04e112281212b87f72f3ea4e3b3db
Lawrence Wakefield <lawrence@thirtymadison.com>
2025-12-09 13:41:41 +0000

    add part of day09

--- 2025/day09.cr
+++ 2025/day09.cr
@@ -39,52 +39,52 @@ def find_start (tiles, map)
 end
 
 # NOTE: doesn't seem like flood filling is a viable option (within 2Gi of RAM at least)
-to_fill = [ find_start(red_tiles, map).not_nil! ]
-until to_fill.empty?
-  x, y = to_fill.shift
-  map[{x,y}] = 'X'
-  [
-      {x, y-1},
-      {x, y+1},
-      {x-1, y},
-      {x+1, y},
-  ].each do |p|
-    to_fill << p unless map[p]?
-  end
-end
+# to_fill = [ find_start(red_tiles, map).not_nil! ]
+# until to_fill.empty?
+#   x, y = to_fill.shift
+#   map[{x,y}] = 'X'
+#   [
+#       {x, y-1},
+#       {x, y+1},
+#       {x-1, y},
+#       {x+1, y},
+#   ].each do |p|
+#     to_fill << p unless map[p]?
+#   end
+# end
 
-minx, maxx = red_tiles.minmax_by(&.[0]).map(&.[0])
+# minx, maxx = red_tiles.minmax_by(&.[0]).map(&.[0])
 
-miny, maxy = red_tiles.minmax_by(&.[1]).map(&.[1])
-((miny - 1)..(maxy + 1)).each do |y|
-  ((minx - 1)..(maxx + 1)).each do |x|
-    STDOUT << (map[{x,y}]? || '.')
-  end
-  STDOUT << '\n'
-end
+# miny, maxy = red_tiles.minmax_by(&.[1]).map(&.[1])
+# ((miny - 1)..(maxy + 1)).each do |y|
+#   ((minx - 1)..(maxx + 1)).each do |x|
+#     STDOUT << (map[{x,y}]? || '.')
+#   end
+#   STDOUT << '\n'
+# end
 
-def is_valid (a, b, map)
-  (miny..maxy).each do |y|
-    (minx..maxx).each do |x|
-    end
-  end
-end
+# def is_valid (a, b, map)
+#   (miny..maxy).each do |y|
+#     (minx..maxx).each do |x|
+#     end
+#   end
+# end
 
-largest = 0u64
-red_tiles.combinations(2).each do |p|
-  minx, maxx = p.map(&.[0]).minmax
-  miny, maxy = p.map(&.[1]).minmax
-  is_valid = (miny..maxy).all? do |y|
-    (minx..maxx).all? do |x|
-      map[{x,y}]?
-    end
-  end
+# largest = 0u64
+# red_tiles.combinations(2).each do |p|
+#   minx, maxx = p.map(&.[0]).minmax
+#   miny, maxy = p.map(&.[1]).minmax
+#   is_valid = (miny..maxy).all? do |y|
+#     (minx..maxx).all? do |x|
+#       map[{x,y}]?
+#     end
+#   end
 
-  next unless is_valid
+#   next unless is_valid
 
-  a = area(p[0], p[1])
-  puts "#{p} -> #{a}"
+#   a = area(p[0], p[1])
+#   puts "#{p} -> #{a}"
 
-  largest = Math.max largest, a
-end
-puts "part 2: #{largest}"
+#   largest = Math.max largest, a
+# end
+# puts "part 2: #{largest}"
